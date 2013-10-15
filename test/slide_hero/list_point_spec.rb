3bbc870f3f57fa0d5532b61ceaccb7f128f70778
Steven Nunez <snunez@cyrusinnovation.com>
2013-10-14 23:07:43 -0400

    Add more tests for ListPoint

--- test/slide_hero/list_point_spec.rb
+++ test/slide_hero/list_point_spec.rb
@@ -3,9 +3,32 @@ require 'minitest_helper'
 module SlideHero
   describe ListPoint do
     it "takes text and an animation on initialization" do
-      line_point = ListPoint.new("point!", animation: nil)
-      line_point.text.must_equal "point!"
-      line_point.animation.must_equal nil
+      list_point = ListPoint.new("point!", animation: nil)
+      list_point.text.must_equal "point!"
+      list_point.animation.must_equal nil
+      list_point.animation_class.must_equal nil
+    end
+
+    describe "#animation_class" do
+      it "applies fragement if animation is true" do
+        list_point = ListPoint.new("foo", animation: true)
+        list_point.animation_class.must_equal " class=\"fragment \""
+      end
+
+      it "only applies simple animation if value not supported" do
+        list_point = ListPoint.new("foo", animation: "banana")
+        list_point.animation_class.must_equal " class=\"fragment \""
+      end
+
+      it "applies extra classes for supported animations" do
+        supported_animations = %w{grow shrink roll-in fade-out 
+      highlight-red highlight-green highlight-blue}
+
+        supported_animations.each do |animation|
+          list_point = ListPoint.new("foo", animation: animation)
+          list_point.animation_class.must_equal " class=\"fragment #{animation}\""
+        end
+      end
     end
   end
 end
